a98f412b007526f455f4054e43998100a604af57
avrahamappel <aappel248@gmail.com>
2020-06-09 23:36:26 -0400

    updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,5 +5,5 @@ alias cls='clear' # Good 'ol Clear Screen command
 alias art="php artisan"
 alias art:ide="php artisan ide-helper:generate && php artisan ide-helper:meta && php artisan ide-helper:models -WR"
 alias art:db="php artisan migrate:fresh --seed"
-alias php@7.2="/usr/local/opt/php@7.2/bin/php"
-alias php@7.3="/usr/local/opt/php@7.3/bin/php"
+alias php72="/usr/local/opt/php@7.2/bin/php"
+alias php73="/usr/local/opt/php@7.3/bin/php"
