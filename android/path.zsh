e526feca5179f4d078ddeefdf144d4aee032ced7
avrahamappel <aappel248@gmail.com>
2020-11-02 00:36:26 -0500

    update android sdk in PATH

--- android/path.zsh
+++ android/path.zsh
@@ -1,1 +1,1 @@
-export PATH="~/Library/Android/sdk/platform-tools/:~/Library/Android/sdk/tools/:$PATH"
+export PATH="~/Library/Android/sdk/platform-tools/:~/Library/Android/sdk/emulator/:~/Library/Android/sdk/tools/:$PATH"
